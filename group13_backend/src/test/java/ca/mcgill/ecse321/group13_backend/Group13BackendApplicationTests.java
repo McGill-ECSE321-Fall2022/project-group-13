155ee0ee8892518585ea1f40c60e3daec5c3c46b
Yu An Lu <yu.a.lu@mail.mcgill.ca>
2022-10-22 01:21:53 -0400

    generated Spring base code

---
+++ group13_backend/src/test/java/ca/mcgill/ecse321/group13_backend/Group13BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.mcgill.ecse321.group13_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Group13BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
