14fa1e3e939c57ae20099d0d08aaf89f8973a979
NikolasPasichnik <nikolas.pasichnik@mail.mcgill.ca>
2022-10-22 21:36:39 -0400

    Generated the java code using the umple extension
    After the previous generation, we had realized that we made a mistake in the enum class, so we had to regenerate the java code.

--- MMS-Backend/src/main/java/ca/mcgill/ecse321/MMSBackend/model/DonationRequest.java
+++ MMS-Backend/src/main/java/ca/mcgill/ecse321/MMSBackend/model/DonationRequest.java
@@ -1,6 +1,6 @@
 /*PLEASE DO NOT EDIT THIS CODE*/
 /*This code was generated using the UMPLE 1.31.1.5860.78bb27cc6 modeling language!*/
-package ca.mcgill.ecse321.MMSBackend.model;
+
 
 import java.util.*;
 
@@ -13,7 +13,7 @@ public class DonationRequest extends Request
   // ENUMERATIONS
   //------------------------
 
-  public enum DonationStatus { Approved, Rejected, Pending, Returned }
+  public enum DonationStatus { Approved, Rejected, Pending }
 
   //------------------------
   // MEMBER VARIABLES
--- MMS-Backend/src/main/java/ca/mcgill/ecse321/MMSBackend/model/LoanRequest.java
+++ MMS-Backend/src/main/java/ca/mcgill/ecse321/MMSBackend/model/LoanRequest.java
@@ -1,6 +1,6 @@
 /*PLEASE DO NOT EDIT THIS CODE*/
 /*This code was generated using the UMPLE 1.31.1.5860.78bb27cc6 modeling language!*/
-package ca.mcgill.ecse321.MMSBackend.model;
+
 
 import java.util.*;
 
@@ -13,7 +13,7 @@ public class LoanRequest extends Request
   // ENUMERATIONS
   //------------------------
 
-  public enum LoanStatus { Approved, Rejected, Pending }
+  public enum LoanStatus { Approved, Rejected, Pending, Returned }
 
   //------------------------
   // MEMBER VARIABLES
