f91b2f525a2933731946e5e17639889f1e71d793
NikolasPasichnik <nikolas.pasichnik@mail.mcgill.ca>
2022-10-22 21:39:36 -0400

    Added the package line
    package ca.mcgill.ecse321.MMSBackend.model;

--- MMS-Backend/src/main/java/ca/mcgill/ecse321/MMSBackend/model/LoanRequest.java
+++ MMS-Backend/src/main/java/ca/mcgill/ecse321/MMSBackend/model/LoanRequest.java
@@ -1,7 +1,7 @@
 /*PLEASE DO NOT EDIT THIS CODE*/
 /*This code was generated using the UMPLE 1.31.1.5860.78bb27cc6 modeling language!*/
 
-
+package ca.mcgill.ecse321.MMSBackend.model;
 import java.util.*;
 
 // line 70 "MMS.ump"
