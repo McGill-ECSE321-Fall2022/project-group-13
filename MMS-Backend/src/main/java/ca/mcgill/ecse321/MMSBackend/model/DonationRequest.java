79b1d0ab77d31a908a1a15db87dbe0931a15e8d7
Yu An Lu <yu.a.lu@mail.mcgill.ca>
2022-10-25 17:41:50 -0400

    Update DonationRequest.java

--- MMS-Backend/src/main/java/ca/mcgill/ecse321/MMSBackend/model/DonationRequest.java
+++ MMS-Backend/src/main/java/ca/mcgill/ecse321/MMSBackend/model/DonationRequest.java
@@ -2,10 +2,12 @@
 /*This code was generated using the UMPLE 1.31.1.5860.78bb27cc6 modeling language!*/
 
 package ca.mcgill.ecse321.MMSBackend.model;
-import java.util.*;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 
 // line 81 "MMS.ump"
 // line 174 "MMS.ump"
+@Entity
 public class DonationRequest extends Request
 {
 
@@ -23,6 +25,7 @@ public class DonationRequest extends Request
   private DonationStatus status;
 
   //DonationRequest Associations
+  @ManyToOne(optional=false)
   private MuseumManagementSystem museumManagementSystem;
 
   //------------------------
