1f7840df7469ffadb2edc666aaae79feab13bb68
naziaC <nazia.chowdhury@mail.mcgill.ca>
2022-10-27 17:44:31 -0400

    Modified Repository for Artifact and LoanRequest

--- MMS-Backend/src/main/java/ca/mcgill/ecse321/MMSBackend/dao/LoanRequestRepository.java
+++ MMS-Backend/src/main/java/ca/mcgill/ecse321/MMSBackend/dao/LoanRequestRepository.java
@@ -3,6 +3,6 @@ package ca.mcgill.ecse321.MMSBackend.dao;
 import org.springframework.data.repository.CrudRepository;
 import ca.mcgill.ecse321.MMSBackend.model.LoanRequest;
 
-public interface LoanRequestRepository extends CrudRepository<LoanRequest, String> {
-    public LoanRequest findLoanRequestByRequestId(String id);
+public interface LoanRequestRepository extends CrudRepository<LoanRequest, Integer> {
+    public LoanRequest findLoanRequestByRequestId(Integer id);
 }
