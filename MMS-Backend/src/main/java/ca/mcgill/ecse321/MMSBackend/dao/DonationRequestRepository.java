7b40cff45e00d8585be54abbab6983b8ba6e0a53
Yu An Lu <yu.a.lu@mail.mcgill.ca>
2022-10-27 17:42:12 -0400

    Update DonationRequestRepository.java
    changed String to Integer

--- MMS-Backend/src/main/java/ca/mcgill/ecse321/MMSBackend/dao/DonationRequestRepository.java
+++ MMS-Backend/src/main/java/ca/mcgill/ecse321/MMSBackend/dao/DonationRequestRepository.java
@@ -3,6 +3,6 @@ package ca.mcgill.ecse321.MMSBackend.dao;
 import org.springframework.data.repository.CrudRepository;
 import ca.mcgill.ecse321.MMSBackend.model.DonationRequest;
 
-public interface DonationRequestRepository extends CrudRepository<DonationRequest, String> {
-    public DonationRequest findDonationRequestByRequestId(String id);
+public interface DonationRequestRepository extends CrudRepository<DonationRequest, Integer> {
+    public DonationRequest findDonationRequestByRequestId(Integer id);
 }
