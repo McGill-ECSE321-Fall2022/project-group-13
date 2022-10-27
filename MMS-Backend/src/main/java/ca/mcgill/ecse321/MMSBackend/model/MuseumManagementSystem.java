43db23225700aa8371e1152c2a929b42907147ba
Lucy Zhang <90661219+Lucy-Zh@users.noreply.github.com>
2022-10-27 13:57:28 -0400

    two imports didnt push for some reason

--- MMS-Backend/src/main/java/ca/mcgill/ecse321/MMSBackend/model/MuseumManagementSystem.java
+++ MMS-Backend/src/main/java/ca/mcgill/ecse321/MMSBackend/model/MuseumManagementSystem.java
@@ -2,7 +2,8 @@
 /*This code was generated using the UMPLE 1.31.1.5860.78bb27cc6 modeling language!*/
 
 package ca.mcgill.ecse321.MMSBackend.model;
-
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
