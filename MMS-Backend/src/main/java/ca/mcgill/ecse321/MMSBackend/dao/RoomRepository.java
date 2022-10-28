175812b58388557707d8776b9aa9bc323cdd3aa1
Mona Kalaoun <mona.kalaoun@mail.mcgill.ca>
2022-10-28 15:44:01 -0400

    Added javadoc to Room related classes

--- MMS-Backend/src/main/java/ca/mcgill/ecse321/MMSBackend/dao/RoomRepository.java
+++ MMS-Backend/src/main/java/ca/mcgill/ecse321/MMSBackend/dao/RoomRepository.java
@@ -3,7 +3,9 @@ package ca.mcgill.ecse321.MMSBackend.dao;
 import org.springframework.data.repository.CrudRepository;
 import ca.mcgill.ecse321.MMSBackend.model.Room;
 
-//Mona
+/**
+ * @author Mona Kalaoun (m-kln)
+ */
 
 public interface RoomRepository extends CrudRepository<Room, Integer>{
     public Room findRoomByRoomId(Integer roomId);
