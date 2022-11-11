c962ea38c6252e1ed94b95b740d0436e69a6ab70
Mona Kalaoun <mona.kalaoun@mail.mcgill.ca>
2022-11-10 21:13:54 -0500

    Updated ArtifactService with new methods, throw exceptions, and javadoc

--- MMS-Backend/src/main/java/ca/mcgill/ecse321/MMSBackend/service/ArtifactService.java
+++ MMS-Backend/src/main/java/ca/mcgill/ecse321/MMSBackend/service/ArtifactService.java
@@ -1,11 +1,15 @@
 package ca.mcgill.ecse321.MMSBackend.service;
 
 import ca.mcgill.ecse321.MMSBackend.dao.*;
+import ca.mcgill.ecse321.MMSBackend.exception.MuseumManagementSystemException;
 import ca.mcgill.ecse321.MMSBackend.model.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.nio.file.InvalidPathException;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,7 +18,7 @@ import java.util.List;
  *
  * The ArtifactService class implements the use case based on the requirement:
  *
- * "The museum management system shall allow the manager and the employees to add or
+ * "FREQ06: The museum management system shall allow the manager and the employees to add or
  * update an artifact by recording the artifact’s name, picture, description, location, loan availability,
  * and loan fee, which can be viewed by all users."
  *
@@ -25,20 +29,36 @@ public class ArtifactService {
     @Autowired
     ArtifactRepository artifactRepository;
 
+    @Autowired
+    RoomRepository roomRepository;
+
+    @Autowired
+    MuseumManagementSystemRepository mmsRepository;
+
     /**
-     * Get a single artifact by its id
-     * @param id
+     * Get a single artifact by its unique id
+     *
+     * @author Mona Kalaoun (m-kln)
+     *
+     * @param id - an int representing the unique id of the artifact
      * @return artifact object
      */
     @Transactional
     public Artifact getArtifact(Integer id){
         Artifact artifact = artifactRepository.findArtifactByArtifactId(id);
+        if (artifact == null){
+            throw new MuseumManagementSystemException(HttpStatus.NOT_FOUND, "Artifact with id: " + id +
+                    " was not found.");
+        }
         return artifact;
     }
 
     /**
      * Get all artifacts (view all artifacts)
-     * @return a list of artifact objects
+     *
+     * @author Mona Kalaoun (m-kln)
+     *
+     * @return a list of all artifact objects in the system
      */
     @Transactional
     public List<Artifact> getAllArtifacts() {
@@ -46,33 +66,134 @@ public class ArtifactService {
     }
 
     /**
+     * Get all artifacts are in the same kind of room
+     *
+     * @author Mona Kalaoun (m-kln)
+     *
+     * @param r - room type from RoomType enum in Room class
+     * @return a list of all artifact objects having the same room type
+     */
+    @Transactional
+    public List<Artifact> getAllArtifactsByRoomType(Room.RoomType r) {
+        List<Artifact> all = toList(artifactRepository.findAll());
+        List<Artifact> result = null;
+
+        for(Artifact a : all){
+            if(a.getRoomLocation().getType().equals(r)){
+                result.add(a);
+            }
+        }
+
+        return result;
+    }
+
+    /**
+     * Get all artifacts that are located in the same specific room
+     *
+     * @author Mona Kalaoun (m-kln)
+     *
+     * @param roomId - an int representing the unique id of the room
+     * @return a list of all artifact objects that are in the same room
+     */
+    @Transactional
+    public List<Artifact> getAllArtifactsByRoomId(int roomId) {
+        List<Artifact> all = toList(artifactRepository.findAll());
+        List<Artifact> result = null;
+
+        for(Artifact a : all){
+            if(a.getRoomLocation().getRoomId() == roomId){
+                result.add(a);
+            }
+        }
+
+        return result;
+    }
+
+    /**
+     * Get all artifacts that share the same loan status
+     *
+     * @author Mona Kalaoun (m-kln)
+     *
+     * @param status - loan status from LoanStatus enum in Artifact class
+     * @return a list of all artifact objects that have the same loan status
+     */
+    @Transactional
+    public List<Artifact> getAllArtifactsByLoanStatus(Artifact.LoanStatus status) {
+        List<Artifact> all = toList(artifactRepository.findAll());
+        List<Artifact> result = null;
+
+        for(Artifact a : all){
+            if(a.getLoanStatus().equals(status)){
+                result.add(a);
+            }
+        }
+
+        return result;
+    }
+
+    /**
+     * Get all artifacts that are in the same state (damaged or not)
+     *
+     * @author Mona Kalaoun (m-kln)
+     *
+     * @param state - a boolean indicating if the artifact is damaged (true) or not (false)
+     * @return a list of all artifact objects that have the same state
+     */
+    @Transactional
+    public List<Artifact> getAllArtifactsByState(boolean state) {
+        List<Artifact> all = toList(artifactRepository.findAll());
+        List<Artifact> result = null;
+
+        for(Artifact a : all){
+            if(a.getIsDamaged() == state){
+                result.add(a);
+            }
+        }
+
+        return result;
+    }
+
+    /**
      * Edit an existing artifact's information
      *
-     * @param artifactId
-     * @param name
-     * @param description
-     * @param status
-     * @param loanFee
-     * @param isDamaged
-     * @param worth
-     * @param newRoom
+     * @author Mona Kalaoun (m-kln)
+     *
+     * @param artifactId - int representing unique id of the artifact to be edited
+     * @param name - String containing the name of the artifact
+     * @param description - String containing the description of the artifact
+     * @param image - String containing the file path of the artifact's image
+     * @param status - LoanStatus enum type indicating the loan status of the artifact
+     * @param loanFee - int representing the fee for the loan
+     * @param isDamaged - boolean indicating if the artifact is damaged or not
+     * @param worth - int representing the monetary value of the artifact
+     * @param roomId - int representing the unique id of the room location of the artifact
+     * @param systemId - int representing the unique id of the current system
+     *
      * @return artifact object once edited
      */
     @Transactional
-    public Artifact editArtifact(Integer artifactId, String name, String description, Artifact.LoanStatus status,
-                                 double loanFee, boolean isDamaged, double worth, Room newRoom){
+    public Artifact editArtifact(int artifactId, String name, String description, String image, Artifact.LoanStatus
+            status, double loanFee, boolean isDamaged, double worth, int roomId, int systemId){
+
+        checkInput(name,description,image,status,loanFee,isDamaged,worth,roomId,systemId);
+
+        if (artifactId < 0) throw new MuseumManagementSystemException(HttpStatus.BAD_REQUEST, "Artifact id can not " +
+                "be negative.");
 
         Artifact artifact = artifactRepository.findArtifactByArtifactId(artifactId);
-        //or Artifact artifact = getArtifact(artifactId);
 
         if(artifact != null){
             artifact.setName(name);
             artifact.setDescription(description);
+            artifact.setImage(image);
             artifact.setLoanStatus(status);
             artifact.setIsDamaged(isDamaged);
             artifact.setLoanFee(loanFee);
             artifact.setWorth(worth);
-            artifact.setRoomLocation(newRoom);
+            artifact.setRoomLocation(Room.getWithRoomId(roomId));
+        }else{
+            throw new MuseumManagementSystemException(HttpStatus.NOT_FOUND, "Artifact with id: " + artifactId +
+                    " was not found.");
         }
 
         return artifact;
@@ -80,42 +201,149 @@ public class ArtifactService {
 
     /**
      * Create an artifact and add it to the system
-     * @param artifact
-     * @return newly created artifact object
+     *
+     * @author Mona Kalaoun (m-kln)
+     *
+     * @param name - String containing the name of the artifact
+     * @param description - String containing the description of the artifact
+     * @param image - String containing the file path of the artifact's image
+     * @param status - LoanStatus enum type indicating the loan status of the artifact
+     * @param loanFee - int representing the fee for the loan
+     * @param isDamaged - boolean indicating if the artifact is damaged or not
+     * @param worth - int representing the monetary value of the artifact
+     * @param roomId - int representing the unique id of the room location of the artifact
+     * @param systemId - int representing the unique id of the current system
+     *
+     * @return a new artifact object
      */
     @Transactional
-    public Artifact addArtifact(Artifact artifact){
-        artifact = artifactRepository.save(artifact);
-        return artifact;
-    }
+    public Artifact createArtifact(String name, String description, String image, Artifact.LoanStatus status,
+                                   double loanFee, boolean isDamaged, double worth, int roomId, int systemId){
 
-    //or
+        checkInput(name, description, image, status, loanFee, isDamaged, worth, roomId, systemId);
 
-    /*@Transactional
-    public Artifact createArtifact(String name, String description, Artifact.LoanStatus status, double loanFee,
-                                   boolean isDamaged, double worth ){
         Artifact artifact = new Artifact();
         artifact.setName(name);
         artifact.setDescription(description);
+        artifact.setImage(image);
         artifact.setLoanStatus(status);
         artifact.setIsDamaged(isDamaged);
         artifact.setLoanFee(loanFee);
         artifact.setWorth(worth);
-        //do I add room location
+        artifact.setRoomLocation(Room.getWithRoomId(roomId));
+        artifact.setMuseumManagementSystem(mmsRepository.findMuseumManagementSystemBySystemId(systemId));
         artifact = artifactRepository.save(artifact);
         return artifact;
-    }*/
+    }
 
     /**
      * Delete an artifact from the system
-     * @param artifactId
+     *
+     * @author Mona Kalaoun (m-kln)
+     *
+     * @param artifactId : int representing the unique id of an existing artifact that is to be deleted
      */
     @Transactional
     public void deleteArtifact(Integer artifactId){
         //check if artifact exists before deleting
         if(artifactRepository.existsById(artifactId)) {
             artifactRepository.deleteById(artifactId);
+        }else {
+            throw new MuseumManagementSystemException(HttpStatus.NOT_FOUND, "Artifact with id: " + artifactId +
+                    " was not found.");
         }
+    }
+
+
+    /**
+     * Checks if a specific room is full
+     *
+     * @author Mona Kalaoun (m-kln)
+     *
+     * @param roomId : int representing the unique id of the room to be checked
+     * @param systemId : int representing the unique id of the system where the room is located
+     */
+    @Transactional
+    public void checkRoom(int roomId, int systemId){
+        //small , 5 with 200 art
+        //large, 5 with 300 art
+        MuseumManagementSystem mms = mmsRepository.findMuseumManagementSystemBySystemId(systemId);
+        if(mms == null) throw new MuseumManagementSystemException(HttpStatus.NOT_FOUND, "The museum with id: " +
+                systemId + " was not found.");
+
+        Room r = roomRepository.findRoomByRoomId(roomId);
+        if(r == null) throw new MuseumManagementSystemException(HttpStatus.NOT_FOUND, "The room with id: " +
+                roomId + " was not found.");
+
+        Room.RoomType roomType = r.getType();
+
+        int count = 0;
+
+        if(mms != null) {
+            for (Artifact a : mms.getArtifacts()) {
+                if (a.getRoomLocation().getRoomId() == roomId) {
+                    count++;
+                }
+            }
+
+            if (r.getType().equals(Room.RoomType.Small)) {
+                if (count >= 1000) {
+                    throw new MuseumManagementSystemException(HttpStatus.CONFLICT, "The selected small room with id: "
+                            + roomId + " is full.");
+                }
+            } else if (r.getType().equals(Room.RoomType.Large)) {
+                if (count >= 1500) {
+                    throw new MuseumManagementSystemException(HttpStatus.CONFLICT, "The selected large room with id: "
+                            + roomId + " is full.");
+                }
+            }
+        }
+    }
+
+
+    /**
+     * Helper method to check the validity of inputs and throw according exceptions
+     *
+     * @author Mona Kalaoun (m-kln)
+     *
+     * @param name - String containing the name of the artifact
+     * @param description - String containing the description of the artifact
+     * @param image - String containing the file path of the artifact's image
+     * @param status - LoanStatus enum type indicating the loan status of the artifact
+     * @param loanFee - int representing the fee for the loan
+     * @param isDamaged - boolean indicating if the artifact is damaged or not
+     * @param worth - int representing the monetary value of the artifact
+     * @param roomId - int representing the unique id of the room location of the artifact
+     * @param systemId - int representing the unique id of the current system
+     */
+    private void checkInput(String name, String description, String image, Artifact.LoanStatus status,
+                            double loanFee, boolean isDamaged, double worth, int roomId, int systemId){
+
+        if( name == null|| description == null || image == null || status == null ||
+                Double.valueOf(loanFee) == null || Boolean.valueOf(isDamaged) == null || Double.valueOf(worth) == null
+                || Integer.valueOf(roomId) == null || Integer.valueOf(systemId) == null){
+            throw new MuseumManagementSystemException(HttpStatus.BAD_REQUEST, "No null values are accepted.");
+        }
+
+        checkRoom(roomId,systemId);
+
+        try {
+            Paths.get(image);
+        } catch(InvalidPathException e) {
+            throw new MuseumManagementSystemException(HttpStatus.BAD_REQUEST, "Image path" + image +" is not valid.");
+        }
+
+        if(loanFee<0) throw new MuseumManagementSystemException(HttpStatus.BAD_REQUEST, "The loan fee can not be " +
+                "a negative value.");
+
+        if(worth<0) throw new MuseumManagementSystemException(HttpStatus.BAD_REQUEST, "The monetary value of the " +
+                "artifact can not be negative.");
+
+        if(roomId<0) throw new MuseumManagementSystemException(HttpStatus.BAD_REQUEST, "The roomId can not be " +
+                "negative.");
+
+        if(systemId<0) throw new MuseumManagementSystemException(HttpStatus.BAD_REQUEST, "The systemId can not be " +
+                "negative.");
     }
 
     /**
@@ -128,5 +356,7 @@ public class ArtifactService {
         }
         return resultList;
     }
+
+
 
 }
