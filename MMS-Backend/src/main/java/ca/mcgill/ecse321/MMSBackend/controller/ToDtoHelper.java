4f63b06f9b9dfefd406633c7cdb7b9fef6c69ca6
Mona Kalaoun <mona.kalaoun@mail.mcgill.ca>
2022-11-11 11:14:42 -0500

    Created helper file where all convertToDto methods will be written

---
+++ MMS-Backend/src/main/java/ca/mcgill/ecse321/MMSBackend/controller/ToDtoHelper.java
@@ -0,0 +1,48 @@
+package ca.mcgill.ecse321.MMSBackend.controller;
+
+import ca.mcgill.ecse321.MMSBackend.model.*;
+import ca.mcgill.ecse321.MMSBackend.dto.*;
+
+public class ToDtoHelper {
+
+    //to be completed
+    private MuseumManagementSystemDto convertToDto(MuseumManagementSystem mms){
+        if (mms == null) {
+            throw new IllegalArgumentException("There is no such Museum Management System!");
+        }
+        MuseumManagementSystemDto system = new MuseumManagementSystemDto();
+        return system;
+    }
+
+    /**
+     * @author Mona Kalaoun (m-kln)
+     * @param r
+     * @return
+     */
+    private RoomDto convertToDto(Room r){
+        if (r == null) {
+            throw new IllegalArgumentException("There is no such Room!");
+        }
+        MuseumManagementSystemDto systemDto = convertToDto(r.getMuseumManagementSystem());
+        RoomDto room = new RoomDto(r.getName(), r.getType(), systemDto);
+        return room;
+    }
+
+    /**
+     * @author Mona Kalaoun (m-kln)
+     * @param a
+     * @return
+     */
+    private ArtifactDto convertToDto(Artifact a) {
+        if (a == null) {
+            throw new IllegalArgumentException("There is no such Artifact!");
+        }
+        RoomDto roomDto = convertToDto(a.getRoomLocation());
+        MuseumManagementSystemDto mmsDto = convertToDto(a.getMuseumManagementSystem());
+
+        ArtifactDto artifactDto = new ArtifactDto(a.getName(),a.getImage(),a.getDescription(),a.getLoanStatus(),
+                a.getIsDamaged(),a.getLoanFee(),a.getWorth(),roomDto, mmsDto);
+
+        return artifactDto;
+    }
+}
