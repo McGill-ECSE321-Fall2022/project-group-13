a5ed0b8362813d45c145698efcb8794190e7c484
NikolasPasichnik <nikolas.pasichnik@mail.mcgill.ca>
2022-11-10 22:43:35 -0500

    Created employee, client and manager DTOs

--- MMS-Backend/src/main/java/ca/mcgill/ecse321/MMSBackend/dto/ClientDto.java
+++ MMS-Backend/src/main/java/ca/mcgill/ecse321/MMSBackend/dto/ClientDto.java
@@ -1,5 +1,62 @@
 package ca.mcgill.ecse321.MMSBackend.dto;
 
 public class ClientDto {
-    
+
+    //Account Attributes
+    private String username;
+    private String name;
+    private String password;
+    private int currentLoanNumber;
+
+    /**
+     * Constructor of a ClientDto
+     * 
+     * @param username
+     * @param name
+     * @param password
+     * @param currentLoanNumber
+     */
+    public ClientDto(String username, String name, String password, int currentLoanNumber){
+        this.username = username; 
+        this.name = name; 
+        this.password = password; 
+        this.currentLoanNumber = currentLoanNumber; 
+    }
+
+    /**
+     * Username getter
+     * 
+     * @return
+     */
+    public String getUsername() {
+        return username; 
+    }
+
+    /**
+     * Name getter
+     * 
+     * @return
+     */
+    public String getName() {
+        return name; 
+    }
+
+    /**
+     * Password getter
+     * 
+     * @return
+     */
+    public String getPassword() {
+        return password; 
+    }
+
+    /**
+     * CurrentLoanNumber getter
+     * 
+     * @return
+     */
+    public int getCurrentLoanNumber() {
+        return currentLoanNumber;
+    }
+
 }
--- MMS-Backend/src/main/java/ca/mcgill/ecse321/MMSBackend/dto/ManagerDto.java
+++ MMS-Backend/src/main/java/ca/mcgill/ecse321/MMSBackend/dto/ManagerDto.java
@@ -1,5 +1,50 @@
 package ca.mcgill.ecse321.MMSBackend.dto;
 
 public class ManagerDto {
+
+    //Account Attributes
+    private String username;
+    private String name;
+    private String password;
     
+    /**
+     * Constructor of a EmployeeDto
+     * 
+     * @param username
+     * @param name
+     * @param password
+     * @param currentLoanNumber
+     */
+    public ManagerDto(String username, String name, String password){
+        this.username = username; 
+        this.name = name; 
+        this.password = password; 
+    }
+
+    /**
+     * Username getter
+     * 
+     * @return
+     */
+    public String getUsername() {
+        return username; 
+    }
+
+    /**
+     * Name getter
+     * 
+     * @return
+     */
+    public String getName() {
+        return name; 
+    }
+
+    /**
+     * Password getter
+     * 
+     * @return
+     */
+    public String getPassword() {
+        return password; 
+    }
 }
