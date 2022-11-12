da5d6ac930aa61994a386eab37d8ff2641e01e5b
NikolasPasichnik <nikolas.pasichnik@mail.mcgill.ca>
2022-11-11 23:46:21 -0500

    Updated DTO classes and updated the account management controller

--- MMS-Backend/src/main/java/ca/mcgill/ecse321/MMSBackend/dto/ManagerDto.java
+++ MMS-Backend/src/main/java/ca/mcgill/ecse321/MMSBackend/dto/ManagerDto.java
@@ -1,11 +1,14 @@
 package ca.mcgill.ecse321.MMSBackend.dto;
 
+import ca.mcgill.ecse321.MMSBackend.model.MuseumManagementSystem;
+
 public class ManagerDto {
 
     //Account Attributes
     private String username;
     private String name;
     private String password;
+    private MuseumManagementSystem MMS; 
     
     // /**
     //  * Empty constructor 
@@ -22,10 +25,11 @@ public class ManagerDto {
      * @param password
      * @param currentLoanNumber
      */
-    public ManagerDto(String username, String name, String password){
+    public ManagerDto(String username, String name, String password, MuseumManagementSystem MMS){
         this.username = username; 
         this.name = name; 
         this.password = password; 
+        this.MMS = MMS; 
     }
 
     /**
@@ -71,5 +75,14 @@ public class ManagerDto {
      */
     public void setPassword(String managerPassword) {
         this.password = managerPassword; 
+    }
+
+    /**
+     * MuseumManagementSystem getter
+     * 
+     * @return
+     */
+    public MuseumManagementSystem getMuseumManagementSystem() {
+        return MMS; 
     }
 }
