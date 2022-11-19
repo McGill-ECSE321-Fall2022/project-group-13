72a1321bfabdb1018a6e551cc032e8a9098bbf65
naziaC <nazia.chowdhury@mail.mcgill.ca>
2022-11-19 14:00:46 -0500

    Update ArtifactRestController.java

--- MMS-Backend/src/main/java/ca/mcgill/ecse321/MMSBackend/controller/ArtifactRestController.java
+++ MMS-Backend/src/main/java/ca/mcgill/ecse321/MMSBackend/controller/ArtifactRestController.java
@@ -4,6 +4,8 @@ import ca.mcgill.ecse321.MMSBackend.model.*;
 import ca.mcgill.ecse321.MMSBackend.dto.*;
 import ca.mcgill.ecse321.MMSBackend.service.ArtifactService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -22,62 +24,58 @@ public class ArtifactRestController {
     private ArtifactService service;
 
     @GetMapping(value = { "/artifacts", "/artifacts/" })
-    public List<ArtifactDto> getAllArtifacts(){
-        return service.getAllArtifacts().stream().map(p -> ToDtoHelper.convertToDto(p)).collect(Collectors.toList());
+    public ResponseEntity<List<ArtifactDto>>  getAllArtifacts(){
+        return new ResponseEntity<List<ArtifactDto>> (service.getAllArtifacts().stream().map(ToDtoHelper::convertToDto).collect(Collectors.toList()), HttpStatus.OK);
     }
 
     @GetMapping(value = { "/artifacts/{id}", "/artifacts/{id}/" })
-    public ArtifactDto getArtifactById(@PathVariable("id") Integer id) throws IllegalArgumentException{
-        return ToDtoHelper.convertToDto(service.getArtifact(id));
+    public ResponseEntity<ArtifactDto> getArtifactById(@PathVariable("id") Integer id) throws IllegalArgumentException{
+        return new ResponseEntity<ArtifactDto> (ToDtoHelper.convertToDto(service.getArtifact(id)), HttpStatus.OK);
     }
 
     @GetMapping(value = { "/artifacts/{roomType}", "/artifacts/{roomType}/" })
-    public List<ArtifactDto> getArtifactsByRoomType(@PathVariable("roomType") Room.RoomType roomType) throws
+    public ResponseEntity<List<ArtifactDto>> getArtifactsByRoomType(@PathVariable("roomType") Room.RoomType roomType) throws
             IllegalArgumentException {
-        return service.getAllArtifactsByRoomType(roomType).stream().map(p ->
-                ToDtoHelper.convertToDto(p)).collect(Collectors.toList());
+        return new ResponseEntity<List<ArtifactDto>> (service.getAllArtifactsByRoomType(roomType).stream().map(ToDtoHelper::convertToDto).collect(Collectors.toList()), HttpStatus.OK);
     }
 
     @GetMapping(value = { "/artifacts/{roomId}", "/artifacts/{roomId}/" })
-    public List<ArtifactDto> getArtifactsByRoomId(@PathVariable("roomId") int roomId) throws
+    public ResponseEntity<List<ArtifactDto>> getArtifactsByRoomId(@PathVariable("roomId") int roomId) throws
             IllegalArgumentException {
-        return service.getAllArtifactsByRoomId(roomId).stream().map(p ->
-                ToDtoHelper.convertToDto(p)).collect(Collectors.toList());
+        return new ResponseEntity<List<ArtifactDto>> (service.getAllArtifactsByRoomId(roomId).stream().map(ToDtoHelper::convertToDto).collect(Collectors.toList()), HttpStatus.OK);
     }
 
     @GetMapping(value = { "/artifacts/{status}", "/artifacts/{status}/" })
-    public List<ArtifactDto> getArtifactsByLoanStatus(@PathVariable("status") Artifact.LoanStatus status) throws
+    public ResponseEntity<List<ArtifactDto>> getArtifactsByLoanStatus(@PathVariable("status") Artifact.LoanStatus status) throws
             IllegalArgumentException {
-        return service.getAllArtifactsByLoanStatus(status).stream().map(p ->
-                ToDtoHelper.convertToDto(p)).collect(Collectors.toList());
+        return new ResponseEntity<List<ArtifactDto>> (service.getAllArtifactsByLoanStatus(status).stream().map(ToDtoHelper::convertToDto).collect(Collectors.toList()), HttpStatus.OK);
     }
 
     @GetMapping(value = { "/artifacts/{state}", "/artifacts/{state}/" })
-    public List<ArtifactDto> getArtifactsByState(@PathVariable("state") boolean state) throws
+    public ResponseEntity<List<ArtifactDto>> getArtifactsByState(@PathVariable("state") boolean state) throws
             IllegalArgumentException {
-        return service.getAllArtifactsByState(state).stream().map(p ->
-                ToDtoHelper.convertToDto(p)).collect(Collectors.toList());
+        return new ResponseEntity<List<ArtifactDto>> (service.getAllArtifactsByState(state).stream().map(ToDtoHelper::convertToDto).collect(Collectors.toList()), HttpStatus.OK);
     }
 
     @PutMapping(value = { "/artifacts/{id}", "/artifacts/{id}/" })
-    public ArtifactDto editArtifact(@PathVariable("id") int artifactId, @RequestParam String name, @RequestParam String
+    public ResponseEntity<ArtifactDto> editArtifact(@PathVariable("id") int artifactId, @RequestParam String name, @RequestParam String
             description, @RequestParam String image, @RequestParam Artifact.LoanStatus status, @RequestParam double
             loanFee, @RequestParam boolean isDamaged, @RequestParam double worth, @RequestParam int roomId,
                                     @RequestParam int systemId) throws IllegalArgumentException{
 
         Artifact artifact = service.editArtifact(artifactId, name, description, image, status, loanFee, isDamaged,
                 worth, roomId, systemId);
-        return ToDtoHelper.convertToDto(artifact);
+        return new ResponseEntity<ArtifactDto> (ToDtoHelper.convertToDto(artifact), HttpStatus.OK);
     }
 
     @PostMapping(value = { "/artifacts/{name}", "/artifacts/{name}/" })
-    public ArtifactDto createArtifact(@PathVariable("name") String name, @RequestParam String description, @RequestParam
+    public ResponseEntity<ArtifactDto> createArtifact(@PathVariable("name") String name, @RequestParam String description, @RequestParam
             String image, @RequestParam Artifact.LoanStatus status, @RequestParam double loanFee, @RequestParam boolean
             isDamaged, @RequestParam double worth, @RequestParam int roomId, @RequestParam int systemId) throws
             IllegalArgumentException{
         Artifact artifact = service.createArtifact(name, description, image, status, loanFee, isDamaged, worth, roomId,
                 systemId);
-        return ToDtoHelper.convertToDto(artifact);
+        return new ResponseEntity<ArtifactDto> (ToDtoHelper.convertToDto(artifact), HttpStatus.OK);
     }
 
     @DeleteMapping(value = { "/artifacts/{id}", "/artifacts/{id}/" })
