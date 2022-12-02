c7abc5cd5e9341e1f1a4e0a0e6b1a46307c9999d
Yu An Lu <yu.a.lu@mail.mcgill.ca>
2022-12-02 13:19:17 -0500

    Update Client.vue

--- MMS-Frontend/src/components/Client.vue
+++ MMS-Frontend/src/components/Client.vue
@@ -6,7 +6,7 @@
             <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
     </head>
     <div class="clientHome">
-        <a href="#/">Hi, Client!</a>
+        <a href="/">Hi, Client!</a>
     </div>
     <div class="topnav">
         <a href="/client/mydonationrequests">Catalog</a>
