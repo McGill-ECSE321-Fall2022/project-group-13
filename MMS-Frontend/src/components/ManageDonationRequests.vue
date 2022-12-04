a250055c4fb7091f923e7566358f4a6754e4d70e
Yu An Lu <yu.a.lu@mail.mcgill.ca>
2022-12-04 13:13:58 -0500

    added get all donations for employee/manager

--- MMS-Frontend/src/components/ManageDonationRequests.vue
+++ MMS-Frontend/src/components/ManageDonationRequests.vue
@@ -5,8 +5,32 @@
             <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
         </head>
         <span class="title">ALL DONATION REQUESTS</span>
+    <span class="title">ALL DONATION REQUESTS</span>
+    <table class="styled-table">
+        <thead>
+            <tr>
+                <th>Donation ID</th>
+                <th>Donator</th>
+                <th>Artifact Name</th>
+                <th>Request Status</th>
+                <th> </th>
+            </tr>
+      </thead>
+      <tbody>
+        <tr v-for="request in requests" :key="request.requestId">
+          <td>{{request.requestId}}</td>
+          <td>{{request.client.username}}</td>
+          <td>{{request.artifact.name}}</td>
+          <td>{{request.status}}</td>
+          <td><button class="styled-button">View</button></td>
+        </tr>
+      </tbody>
+    </table>
     </div>
 </template>
+
+<script src="./script/ManageDonationRequests.js">
+</script>
 
 <style scoped>
 .myDonations {
