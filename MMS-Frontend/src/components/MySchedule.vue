e2d27aaca23cf41cc62645068a0dfce32c29bfcb
samperezh <samantha.perezhoffman@mail.mcgill.ca>
2022-12-03 19:07:11 -0500

    created MySchedule page

--- MMS-Frontend/src/components/MySchedule.vue
+++ MMS-Frontend/src/components/MySchedule.vue
@@ -5,8 +5,66 @@
             <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
         </head>
         <span class="title">MY SCHEDULE</span>
+        <table class="styled-table">
+        <thead>
+          <tr>
+            <th>Day</th>
+            <th>Start Time</th>
+            <th>End Time</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="s in shifts" :key="s.shiftId">
+                <td>{{s.specificWeekDay.dayType}}</td>
+                <td>{{s.startTime}}</td>
+                <td>{{s.endTime}}</td>
+            </tr>
+        </tbody>
+    </table>   
     </div>
 </template>
+
+<script>
+import axios from 'axios';
+
+var config = require('../../config')
+var frontendUrl = 'http://' + config.dev.host + ':' + config.dev.port
+var backendUrl = 'http://' + config.dev.backendHost + ':' + config.dev.backendPort
+
+var axiosEmployee = axios.create({
+    baseURL: backendUrl,
+    headers: { 'Access-Control-Allow-Origin': frontendUrl }
+})
+
+export default {
+  name: 'MySchedule',
+  data(){
+    return {
+      employeeUsername: '',
+      shifts: [], 
+      mondayShifts: '',
+    }
+  }, 
+  created(){
+    this.employeeUsername = sessionStorage.getItem('loggedInEmployee');
+    console.log(this.employeeUsername);
+    axiosEmployee.get('/shift/employee/', {
+      params: {
+        employeeUsername: this.employeeUsername
+      }
+    })
+    .then(response => {
+            console.log(response)
+            this.shifts = response.data
+        })
+    .catch(e => {
+          console.log('Error in GET /shift/employee')
+          console.log(e)
+        })
+  }
+}
+
+</script>
 
 <style scoped>
 .mySchedule {
@@ -20,22 +78,31 @@
   overflow: hidden;
 }
 
-.bottomText {
-  position: fixed;
-  text-align: center;
-  padding-bottom: 40px;
-  left: 0;
-  bottom: 0;
-  right: 0;
+.styled-table {
+    border-collapse: collapse;
+    margin: 200px 60px;
+    font-size: 25px;
+    font-family: sans-serif;
+    min-width: 90%;
+    box-shadow: 0 0 20px rgba(0, 0, 0, 0.15);
 }
 
-.thankYou {
-  width: 796px;
-  color: rgba(0,0,0,1);
-  font-family: Inter;
-  font-weight: Bold;
-  font-size: 40px;
-  opacity: 1;
-  text-align: center;
+.styled-table th,
+.styled-table td {
+    padding: 12px 15px;
 }
+
+.styled-table thead tr {
+    background-color: #008573;
+    color: #ffffff;
+    text-align: middle;
+}
+.styled-table tbody tr {
+    border-bottom: 1px solid #dddddd;
+}
+
+.styled-table tbody tr:nth-of-type(even) {
+    background-color: #f3f3f3;
+}
+
 </style>
