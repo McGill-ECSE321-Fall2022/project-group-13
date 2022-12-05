4960d61072572ba0566742772b253a2e4ceb8b8d
samperezh <samantha.perezhoffman@mail.mcgill.ca>
2022-12-04 19:22:21 -0500

    edited table header

--- MMS-Frontend/src/components/viewSchedules.vue
+++ MMS-Frontend/src/components/viewSchedules.vue
@@ -62,7 +62,7 @@ export default {
     data(){
         return {
             shifts: [],
-            selectedDay: 'Monday',
+            selectedDay: 'PickDay',
         }
     }, 
     created(){
